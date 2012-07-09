b533896ab2c0b181943fae9be791ca218f47f508
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-07-09 12:02:32 -0500

    Used generator instead, bit faster

--- p_4.py
+++ p_4.py
@@ -5,5 +5,7 @@ made from the product of two
 '''
 
 is_palindrome = lambda x: str(x) == str(x)[::-1]
-number_range = [y * x for x in xrange(100, 999) for y in xrange(x, 999)]
-max(filter(is_palindrome, number_range))  # 906609
+number_range = (y * x for x in xrange(100, 999) for y in xrange(x, 999))
+answer = max(filter(is_palindrome, number_range))  # 906609
+
+print answer
