1982b7ad6fefdd59145e32bc7e1ef296261634c4
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-07-09 11:33:52 -0500

    finished a few easy problems

---
+++ p_4.py
@@ -0,0 +1,9 @@
+'''a
+Find the largest palindrome
+made from the product of two
+3-digit numbers
+'''
+
+is_palindrome = lambda x: str(x) == str(x)[::-1]
+number_range = [y * x for x in xrange(100, 999) for y in xrange(x, 999)]
+max(filter(is_palindrome, number_range))  # 906609
