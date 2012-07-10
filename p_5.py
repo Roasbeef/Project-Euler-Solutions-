6dcc47a40b711c5a05901f23e2667305b214af2f
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-07-10 03:02:49 -0500

    completed problem 5

---
+++ p_5.py
@@ -0,0 +1,37 @@
+############
+# version 1#
+############
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a % b)
+
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+#fold list calculating lcm of each pair
+result = reduce(lcm, range(2, 21))
+
+#############
+# version 2 #
+#############
+def prime_factors(n):
+    factors = [1]
+    last = n
+
+    while last > 1:
+        c = 2
+        while last % c > 0:
+            c += 1
+
+        factors.append(c)
+        last /= c
+    return factors
+
+#generate list of lcms of prime factorizations of 11-20
+lcms = map(prime_factors, range(11, 21))
+#remove 12 to avoid duplicates
+lcms = lcms[:1] + lcms[2:]
+#multiply highest power of each prime factorization cutting off a repeat
+lcms = sorted([max(x) ** x.count(max(x)) for x in lcms])[1:]
+from operator import mul
+result = reduce(mul, lcms)
