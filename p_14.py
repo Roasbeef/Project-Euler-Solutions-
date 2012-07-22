a15fa3fcc2277650e7bfa1a3479081a91cbe03ab
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-07-21 22:58:25 -0500

    caching for 14 much faster

--- p_14.py
+++ p_14.py
@@ -3,16 +3,28 @@ Which starting number, under one million,
 produces the longest chain?
 '''
 
+cache = {}
 
-def len_collatz(num):
-    seq = [num]
-    while seq[-1] > 1:
-        if not seq[-1] % 2:
-            seq.append(seq[-1] / 2)
+
+def len_collatz(starting_num):
+    steps = 0
+    current_num = starting_num
+    while True:
+        if current_num in cache:
+            cache[starting_num] = steps + cache[current_num]
+            return steps + cache[current_num]
+        elif current_num == 1:
+            break
+        elif not current_num % 2:
+            current_num = current_num / 2
         else:
-            seq.append((3 * seq[-1]) + 1)
-    return len(seq), num
+            current_num = (3 * current_num) + 1
+        steps += 1
 
-answer = max([len_collatz(x) for x in xrange(1000001)])[1]
+    cache[starting_num] = steps + 1
+    return steps + 1
+
+
+answer = max(((len_collatz(x), x) for x in xrange(1, 1000001)))
 
 print answer  # 837799
